a33832a511bc7387698aa4fd726b12566391e2cf
Stewart McSporran <stewart@amiwill.co.uk>
2018-10-21 11:45:11 +0100

    Added boilerplate project files.

---
+++ AdmiraltyTidalApiClient/AdmiraltyTidalApiClient.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		AdmiraltyTidalApiClient.cpp
+ Created:	10/21/2018 10:43:30 AM
+ Author:	Stewart
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "AdmiraltyTidalApiClient.h"
+
+
---
+++ AdmiraltyTidalApiClient/AdmiraltyTidalApiClient.h
@@ -0,0 +1,19 @@
+/*
+ Name:		AdmiraltyTidalApiClient.h
+ Created:	10/21/2018 10:43:30 AM
+ Author:	Stewart
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _AdmiraltyTidalApiClient_h
+#define _AdmiraltyTidalApiClient_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
